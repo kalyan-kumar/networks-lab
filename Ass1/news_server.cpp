8f902f199a333a8698fbf394584e55a0a06dc99f
Kalyan Kumar <kalyankumar.garapaty@gmail.com>
2016-01-26 01:21:42 +0530

    Finish reader functionality.
    Signed-off-by: Kalyan Kumar <kalyankumar.garapaty@gmail.com>

--- Ass1/news_server.cpp
+++ Ass1/news_server.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -9,146 +10,190 @@
 #include "news_header.h"
 #include <dirent.h>
 
+using namespace std;
+
+
+/*bool sortDayMonthYear(const news firstnews, const news secondnews)
+{
+    string first = firstnews.date;
+    string second = secondnews.date;
+    int dd1, dd2, mm1, mm2, yy1, yy2;
+    sscanf(first.c_str(), "%d/%d/%d", &dd1, &mm1, &yy1);
+    sscanf(second.c_str(), "%d/%d/%d", &dd2, &mm2, &yy2);
+    // Algorithm to find out which string is "lesser" than the other
+    if(yy1 > yy2)
+        return true;
+    else if(yy1 < yy2)
+        return false;
+    else if(yy1 == yy2)
+        if(mm1 > mm2)
+            return true;
+        else if(mm1 < mm2)
+            return false;
+        else if(mm1 == mm2)
+            if(dd1 >= dd2)
+                return true;
+            else
+                return false;
+}*/
+
+article academic[100], nacademic[100];
+
+char *getfiles(char direct[1000], bool flag)
+{
+	int i;
+	char buf[1000] ;
+	DIR *d;
+	struct dirent *dp;
+	article tmp;
+	d = opendir(direct);
+	strcpy(buf, "");
+	if(d)
+	{
+		for(i=1;(dp=readdir(d))!=NULL;i++)
+		{
+			if(strcmp(dp->d_name, ".")==0 || strcmp(dp->d_name, "..")==0)
+				continue;
+			strcat(buf, "\n");
+			strcat(buf, to_string(i).c_str());
+			strcat(buf, " ");
+			if(flag)
+			{
+				academic[i].readFromFile(dp->d_name, true);
+				strcat(buf, academic[i].heading.c_str());
+			}
+			else
+			{
+				nacademic[i].readFromFile(dp->d_name, true);
+				strcat(buf, nacademic[i].heading.c_str());
+			}
+		}
+	}
+	else
+		perror("Cant open directory");
+	closedir(d);
+	return buf;
+}
+
 void handleReader(int cfd)
 {
 	int siz;
 	string path1;
-	char buf1[1000] = {'\0'}, buf2[1000] = {'\0'}, direct[1000], path[1000];
-	if(recv(cfd, buf1, 1000, 0) == -1)
-	{
-		printf("Didnt get what the client requested for\n");
-		exit(1);
-	}
+	article tmp;
+	char buf1[1000] = {'\0'}, buf2[1000] = {'\0'}, direct[1000] = {'\0'}, path[1000] = {'\0'}, paths[1000] = {'\0'}, paths1[1000] = {'\0'};
 	getcwd(direct, 1000);
-	DIR *d;
-	struct dirent *dp;
-	if(!strcmp(buf1, "Academic"))
+	strcpy(path, direct);
+	strcat(path, "/Data/Academic");
+	strcpy(buf1, getfiles(path, true));
+	strcpy(path, direct);
+	strcat(path, "/Data/Non-Academic");
+	strcpy(buf2, getfiles(path, false));
+	
+	if(send(cfd, "Welcome to the database!", 24, 0) == -1)
 	{
-		strcat(direct, "/Data/Academic");
-		article tmp;
-		printf("%s\n", direct);
-		d = opendir(direct);
-		if(d)
-		{
-			while((dp=readdir(d))!=NULL)
-			{
-				tmp.readFromFile(dp->d_name, true);
-				siz = tmp.heading.size();
-				if(send(cfd, tmp.heading.c_str(), siz, 0) == -1)
-				{
-					perror("Server write failed");
-					exit(1);
-				}
-			}
-		}
-		else
-			perror("Cant open directory");
-		closedir(d);
+		perror("Server write failed");
+		exit(1);
 	}
-	else
-	{
-		strcat(direct, "/Data/Non-Academic");
-		article tmp;
-		d = opendir(direct);
-		if(d)
-		{
-			while((dp=readdir(d))!=NULL)
-			{
-				tmp.readFromFile(dp->d_name, false);
-				tmp.print();
-				siz = tmp.heading.size();
-				if(send(cfd, tmp.heading.c_str(), siz, 0) == -1)
-				{
-					perror("Server write failed");
-					exit(1);
-				}
-			}
-		}
-		else
-			perror("Cant open directory");
-		closedir(d);
-	}
-	if(recv(cfd, buf2, 1000, 0) == -1)
+	
+	if(recv(cfd, paths, 1000, 0) == -1)
 	{
 		printf("Didnt get what the client requested for\n");
 		exit(1);
 	}
-	if(!strcmp(buf1, "Academic"))
+	if(!strcmp(paths, "Academic"))
 	{
-		strcat(direct, "/Data/Academic");
-		article tmp;
-		d = opendir(direct);
-		if(d)
+		if(send(cfd, buf1, strlen(buf1), 0) == -1)
 		{
-			while((dp=readdir(d))!=NULL)
-			{
-				tmp.readFromFile(dp->d_name, true);
-				if(!strcmp(tmp.heading.c_str(), buf2))
-				{
-					siz = tmp.heading.size();
-					if(send(cfd, tmp.heading.c_str(), siz, 0) == -1)
-					{
-						perror("Server write failed");
-						exit(1);
-					}
-					siz = tmp.date.size();
-					if(send(cfd, tmp.date.c_str(), siz, 0) == -1)
-					{
-						perror("Server write failed");
-						exit(1);
-					}
-					siz = tmp.text.size();
-					if(send(cfd, tmp.text.c_str(), siz, 0) == -1)
-					{
-						perror("Server write failed");
-						exit(1);
-					}
-					break;
-				}
-			}
+			perror("Server write failed");
+			exit(1);
 		}
-		else
-			perror("Cant open directory");
-		closedir(d);
 	}
 	else
 	{
-		strcat(direct, "/Data/Non-Academic");
-		article tmp;
-		d = opendir(direct);
-		if(d)
+		if(send(cfd, buf2, strlen(buf2), 0) == -1)
 		{
-			strcpy(path, direct);
-			while((dp=readdir(d))!=NULL)
-			{
-				tmp.readFromFile(dp->d_name, false);
-				if(!strcmp(tmp.heading.c_str(), buf2))
-				{
-					siz = tmp.heading.size();
-					if(send(cfd, tmp.heading.c_str(), siz, 0) == -1)
-					{
-						perror("Server write failed");
-						exit(1);
-					}
-					siz = tmp.date.size();
-					if(send(cfd, tmp.date.c_str(), siz, 0) == -1)
-					{
-						perror("Server write failed");
-						exit(1);
-					}
-					siz = tmp.text.size();
-					if(send(cfd, tmp.text.c_str(), siz, 0) == -1)
-					{
-						perror("Server write failed");
-						exit(1);
-					}
-					break;
-				}
-			}
+			perror("Server write failed");
+			exit(1);
 		}
-		else
-			perror("Cant open directory");
-		closedir(d);
+	}
+	if(recv(cfd, paths1, 1000, 0) == -1)
+	{
+		printf("Didnt get what the client requested for\n");
+		exit(1);
+	}
+	if(!strcmp(paths, "Academic"))
+	{
+		tmp = academic[stoi(paths1)];
+		siz = tmp.heading.size();
+		if(send(cfd, tmp.heading.c_str(), siz, 0) == -1)
+		{
+			perror("Server write failed");
+			exit(1);
+		}
+		if(recv(cfd, paths1, 1000, 0) == -1)
+		{
+			printf("Didnt get what the client requested for\n");
+			exit(1);
+		}
+		siz = tmp.date.size();
+		if(send(cfd, tmp.date.c_str(), siz, 0) == -1)
+		{
+			perror("Server write failed");
+			exit(1);
+		}
+		if(recv(cfd, paths1, 1000, 0) == -1)
+		{
+			printf("Didnt get what the client requested for\n");
+			exit(1);
+		}
+		siz = tmp.text.size();
+		if(send(cfd, tmp.text.c_str(), siz, 0) == -1)
+		{
+			perror("Server write failed");
+			exit(1);
+		}
+		if(recv(cfd, paths1, 1000, 0) == -1)
+		{
+			printf("Didnt get what the client requested for\n");
+			exit(1);
+		}
+	}
+	else
+	{
+		tmp = nacademic[stoi(paths1)];
+		siz = tmp.heading.size();
+		if(send(cfd, tmp.heading.c_str(), siz, 0) == -1)
+		{
+			perror("Server write failed");
+			exit(1);
+		}
+		if(recv(cfd, paths1, 1000, 0) == -1)
+		{
+			printf("Didnt get what the client requested for\n");
+			exit(1);
+		}
+		siz = tmp.date.size();
+		if(send(cfd, tmp.date.c_str(), siz, 0) == -1)
+		{
+			perror("Server write failed");
+			exit(1);
+		}
+		if(recv(cfd, paths1, 1000, 0) == -1)
+		{
+			printf("Didnt get what the client requested for\n");
+			exit(1);
+		}
+		siz = tmp.text.size();
+		if(send(cfd, tmp.text.c_str(), siz, 0) == -1)
+		{
+			perror("Server write failed");
+			exit(1);
+		}
+		if(recv(cfd, paths1, 1000, 0) == -1)
+		{
+			printf("Didnt get what the client requested for\n");
+			exit(1);
+		}
 	}
 
 }
@@ -228,6 +273,7 @@ void handleClient(int cfd)
 			handleReporter(cfd);
 		else if(!strcmp(buf, "Administrator"))
 			handleAdministrator(cfd);
+		printf("Successfully finished a request\n");
 		exit(1);
 	}
 	else if(pidc > 0)
@@ -256,7 +302,7 @@ int main(int argc, char **argv)
 	printf("Socket fd = %d\n", sfd);
 	
 	srv_addr.sin_family = AF_INET;
-	srv_addr.sin_port   = htons(21434);
+	srv_addr.sin_port   = htons(21435);
 	if(inet_pton(AF_INET, "127.0.0.1", &srv_addr.sin_addr) <= 0)
 	{
 		perror("Network address conversion.\n");
